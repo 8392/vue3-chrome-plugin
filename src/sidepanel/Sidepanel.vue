d085d024c518b4b8af372fd23a1bf6ea4884f145
caibo <11699221+dytcgitee@user.noreply.gitee.com>
2025-09-19 15:07:30 +0800

    更新

--- src/sidepanel/Sidepanel.vue
+++ src/sidepanel/Sidepanel.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+const inputVal = ref('')
+
 function openOptionsPage() {
   browser.runtime.openOptionsPage()
 }
@@ -6,6 +8,13 @@ function openOptionsPage() {
 
 <template>
   <main class="w-full px-4 py-5 text-center text-gray-700">
+    <div>
+      <input v-model="inputVal" class="px-[5px] h-[36px] border-1px border-[#ccc]">
+      <div>
+        inputVasss测试l：{{ inputVal }}
+      </div>
+    </div>
+
     <button class="btn mt-2" @click="openOptionsPage">
       打开options页面
     </button>
